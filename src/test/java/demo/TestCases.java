7af8ce4d199125e74363cc4ca2b15aa4a7aad8a1
Virendra Singh <singh98virendra@gmail.com>
2025-03-18 18:56:23 +0530

    ME_QA_XYOUTUBE_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,6 +1,8 @@
 package demo;
 
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,25 +19,161 @@ import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.List;
+import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 
 import demo.utils.ExcelDataProvider;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
-public class TestCases extends ExcelDataProvider{ // Lets us read the data
+public class TestCases extends ExcelDataProvider { // Lets us read the data
         ChromeDriver driver;
 
-        /*
-         * TODO: Write your tests here with testng @Test annotation.
-         * Follow `testCase01` `testCase02`... format or what is provided in
-         * instructions
-         */
+        @Test
+        public void testCase01() {
+                Wrappers.navigate(driver, "https://www.youtube.com/");
+                SoftAssert softAssert = new SoftAssert();
+                String currentURL = driver.getCurrentUrl();
+                softAssert.assertTrue(currentURL.contains("youtube"), "URL doesn't contains Youtube");
+                WebElement hamBurgerSign = driver.findElement(By.id("guide-button"));
+                Wrappers.click(hamBurgerSign, driver);
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                WebElement aboutLink = wait
+                                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[text()='About']")));
+                Wrappers.click(aboutLink, driver);
+                WebElement aboutHeading = wait.until(ExpectedConditions
+                                .visibilityOfElementLocated(By.xpath("//main[@id='content']/section/h1")));
+                String aboutHeadingText = aboutHeading.getText();
+                softAssert.assertTrue(aboutHeadingText.contains("About"), "Heading doesn't contains About");
+                softAssert.assertAll();
+        }
 
-        /*
-         * Do not change the provided methods unless necessary, they will help in
-         * automation and assessment
-         */
+        @Test
+        public void testCase02() throws InterruptedException {
+
+                Wrappers.navigate(driver, "https://www.youtube.com/");
+                SoftAssert softAssert = new SoftAssert();
+                String currentURL = driver.getCurrentUrl();
+                softAssert.assertTrue(currentURL.contains("youtube"), "URL doesn't contains Youtube");
+                WebElement hamBurgerSign = driver.findElement(By.id("guide-button"));
+                Wrappers.click(hamBurgerSign, driver);
+
+                String movies = "Movies";
+                WebElement moviesButton = driver
+                                .findElement(By.xpath("//yt-formatted-string[text()='" + movies + "']"));
+                Wrappers.click(moviesButton, driver);
+
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3));
+                wait.until(ExpectedConditions.urlContains("feed"));
+                Thread.sleep(3000);
+                WebElement nextButton = driver.findElement(By.xpath(
+                                "//span[text()='Top selling']/ancestor::div[@id='dismissible']//button[@aria-label='Next']"));
+                Boolean flag = true;
+                while (flag) {
+                        
+                        if (!nextButton.isDisplayed()) {
+                                flag = false; // Exit the loop when no more "next" button is visible
+                        }
+                        else{
+                              Wrappers.click(nextButton, driver);
+                        }   
+                }
+                System.out.println("Reached last movie tile");
+
+                WebElement lastMovieGenre = driver.findElement(
+                                By.xpath("(//span[contains(@class,'ytd-grid-movie-renderer')])[position()=last()]"));
+                String lastMovieGenreText = lastMovieGenre.getText();
+                softAssert.assertTrue(lastMovieGenreText.contains("Comedy"), "Doesn't contains Comedy");
+                softAssert.assertTrue(lastMovieGenreText.contains("Animation"), "Doesn't contains Animation");
+                softAssert.assertTrue(lastMovieGenreText.contains("Drama"), "Doesn't contains Drama");
+                WebElement lastAdultRating = driver.findElement(
+                                By.xpath("(//p[contains(@class,'ytd-badge-supported-renderer')])[position()=last()]"));
+                String lastAdultRatingText = lastAdultRating.getText();
+                softAssert.assertTrue(lastAdultRatingText.equals("A"), "Rating is not A");
+
+                softAssert.assertAll();
+
+        }
+
+        @Test
+        public void testCase03() throws InterruptedException {
+
+                Wrappers.navigate(driver, "https://www.youtube.com/");
+                SoftAssert softAssert = new SoftAssert();
+                String currentURL = driver.getCurrentUrl();
+                softAssert.assertTrue(currentURL.contains("youtube"), "URL doesn't contains Youtube");
+                WebElement hamBurgerSign = driver.findElement(By.id("guide-button"));
+                Wrappers.click(hamBurgerSign, driver);
+
+                String music = "Music";
+                WebElement musicButton = driver.findElement(By.xpath("//yt-formatted-string[text()='" + music + "']"));
+                Wrappers.click(musicButton, driver);
+
+                
+                Thread.sleep(3000);
+                WebElement lastMusicTileSongs = driver.findElement(By.xpath(
+                                "//span[text()=\"India's Biggest Hits\"]/ancestor::div[@id='dismissible']//div[@id='contents']//ytd-rich-item-renderer[4]//div[@class='badge-shape-wiz__text']"));
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("var rect = arguments[0].getBoundingClientRect();" +
+                                "window.scrollTo({ top: rect.top + window.pageYOffset - (window.innerHeight / 2), behavior: 'smooth' });",
+                                lastMusicTileSongs);
+                int numberOfSongsInPlaylist = Integer.parseInt(lastMusicTileSongs.getText().split(" ")[0]);
+                softAssert.assertTrue(numberOfSongsInPlaylist >= 50, "Number of songs are more than 50");
+                softAssert.assertAll();
+                Thread.sleep(3000);
+
+        }
+
+        @Test
+        public void testCase04() throws InterruptedException {
+
+                
+                Wrappers.navigate(driver, "https://www.youtube.com/");
+                SoftAssert softAssert = new SoftAssert();
+                String currentURL = driver.getCurrentUrl();
+                softAssert.assertTrue(currentURL.contains("youtube"), "URL doesn't contains Youtube");
+                WebElement hamBurgerSign = driver.findElement(By.id("guide-button"));
+                Wrappers.click(hamBurgerSign, driver);
+
+                String news = "News";
+                WebElement newsButton = driver.findElement(By.xpath("//yt-formatted-string[text()='" + news + "']"));
+                Wrappers.click(newsButton, driver);
+
+                WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+                WebElement newTiles = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//span[text()='Latest news posts']/ancestor::div[@id='dismissible']//span[@id='vote-count-middle']")));
+                JavascriptExecutor js = (JavascriptExecutor) driver;
+                js.executeScript("arguments[0].style.display='block'", newTiles);
+
+                List<WebElement> newsTilesLikes = driver.findElements(By.xpath("//span[text()='Latest news posts']/ancestor::div[@id='dismissible']//span[@id='vote-count-middle']"));
+                int limit = 3;
+                int sumOfLikes = 0;
+                for(WebElement element : newsTilesLikes){
+                        if(limit>0){
+                               
+                                String likesCount = element.getText().replaceAll("[^\\d]", "");
+                                try{
+                                        sumOfLikes += Integer.parseInt(likesCount);
+                                        System.out.println(likesCount);
+                                }
+                                catch(Exception e){
+                                        sumOfLikes += 0;
+                                        System.out.println(0);
+                                }
+                                limit--;
+                        }
+                }
+
+                System.out.println("Total like of Three tiles: "+ sumOfLikes);
+                Thread.sleep(3000);
+
+        }
+
+        @Test
+        public void testCase05() {
+
+        }
+
         @BeforeTest
         public void startBrowser() {
                 System.setProperty("java.util.logging.config.file", "logging.properties");
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,17 +1,58 @@
 package demo.wrappers;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 public class Wrappers {
-    /*
-     * Write your selenium wrappers here
-     */
+    public static void click(WebElement elementToClick, WebDriver driver) {
+        if (elementToClick.isDisplayed()) {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("var rect = arguments[0].getBoundingClientRect();" +
+                    "window.scrollTo({ top: rect.top + window.pageYOffset - (window.innerHeight / 2), behavior: 'smooth' });",
+                    elementToClick);
+            elementToClick.click();
+        }
+    }
+
+    public static void sendKeys(WebDriver driver, WebElement inputBox, String keysToSend) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("var rect = arguments[0].getBoundingClientRect();" +
+                "window.scrollTo({ top: rect.top + window.pageYOffset - (window.innerHeight / 2), behavior: 'smooth' });",
+                inputBox);
+        inputBox.clear();
+        inputBox.sendKeys(keysToSend);
+    }
+
+    public static void navigate(WebDriver driver, String url) {
+        if (!driver.getCurrentUrl().equals(url)) {
+            driver.get(url);
+        }
+    }
+
+    public static WebElement findElementWithRetry(WebDriver driver, By by, int retryCount) {
+        return driver.findElement(by);
+    }
+
+    public static String capture(WebDriver driver) throws IOException {
+        File srcFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+        File dest = new File(
+                System.getProperty("user.dir") + File.separator + "reports" + System.currentTimeMillis() + ".png");
+        String errflPath = dest.getAbsolutePath();
+        FileUtils.copyFile(srcFile, dest);
+        return errflPath;
+    }
 }
